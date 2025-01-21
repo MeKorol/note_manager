120f125583b9310faac9289028bf7d7450ab326f
Bravo_One <63002887+MeKorol@users.noreply.github.com>
2025-01-21 20:27:43 +0300

    date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -1,8 +1,8 @@
 #Уже записанная дата
 created_date = "10.12.2025"
 print("Дата создания заметки: ",created_date)
-print("Дата создания заметки без года: ",created_date[0:6])
+print("Дата создания заметки без года: ",created_date[:5])
 #Ввод даты
 created_date_input = (input("\nВведите дату (дд.мм.гг): "))
 print("Дата создания заметки: ",created_date_input)
-print("Дата создания заметки без года: ",created_date_input[0:6])
+print("Дата создания заметки без года: ",created_date_input[:5])
